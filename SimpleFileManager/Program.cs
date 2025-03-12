e4c5e30903236dc42ba7ac61a208eeb52bfa14f4
vardikore <4erepaxel@gmail.com>
2025-03-12 20:16:39 +0300

    Add files via upload

---
+++ SimpleFileManager/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class FileManager
+{
+        FileManager manager = new FileManager();
+        manager.LoadState();
+        while (true)
+        {
+            Console.Write("> ");
+            string input = Console.ReadLine();
+            string[] command = input.Split(' ');
+            switch (command[0])
+            {
+                case "ls":
+                    manager.ListFiles();
+                    break;
+                case "cd":
+                    manager.ChangeDirectory(command[1]);
+                    break;
+                case "cp":
+                    manager.CopyFile(command[1], command[2]);
+                    break;
+                case "rm":
+                    manager.DeleteFile(command[1]);
+                    break;
+                case "info":
+                    manager.GetFileInfo(command[1]);
+                    break;
+                case "exit":
+                    manager.SaveState();
+                    return;
+                default:
+                    Console.WriteLine("Unknown command.");
+                    break;
+            }
+        }
+    }
